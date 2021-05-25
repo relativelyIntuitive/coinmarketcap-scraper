c9a1dbe68078a4acce4d60b26b36be5f77ca456e
PhlankStake <z.bielicki88@gmail.com>
2021-05-25 19:06:13 -0400

    ~QOL print statements to track progress +dt stamp for .CSV +sqlite support!

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 "# coinmarketcap-scraper" 
 
-A simple scraping program I coded to pull market data for the top 100 currencies listed on the homepage of CoinMarketCap.com. The results will be saved to a .csv file in the root directory.
+A simple scraping program I coded to pull market data for the top 100 currencies listed on the homepage of CoinMarketCap.com. The results will be saved to a SQLite database, as well a .CSV file created in the root directory.
 
 1. Ensure that Python3 AND Chrome browser are properly installed on your system
 2. Create a virtual environment within the root directory to install dependencies
@@ -9,8 +9,9 @@ A simple scraping program I coded to pull market data for the top 100 currencies
 4. If on a Mac, modify line 10 of "scraper.py" to reference "chromedriver" without the ".exe"
 5. The "chromedriver.exe" file is for the Windows edition of Chrome version 90, if you have a different version of chrome installed, download the proper driver from "https://chromedriver.chromium.org/downloads", unzip the package, and replace "chromedriver.exe" with the extracted executable
 6. Run "scraper.py" with Python3 from the root directory
-7. The program will run for less than 30 seconds
-8. A .csv file will be created within the root directory, containing the results! 
+7. The program will run for less than 30 seconds, progress can be tracked in the terminal!
+8. A .CSV file will be created within the root directory, containing the results!
+9. The database will now contain the timestamped results! 
 
 NO LICENSE! THIS CODE IS NOT MEANT TO BE REUSED OR REPRODUCED!
 Copyright ©2021 Zachery A. Bielicki, all rights reserved
--- scraper.py
+++ scraper.py
@@ -2,9 +2,34 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import datetime
 import time
+import sqlite3
+import os.path
 
+# creates database file if none exist and configures database connection
+if os.path.exists('coinmarketcap_scraper_db.db'):
+    try:
+        dbConn = sqlite3.connect('coinmarketcap_scraper_db.db')
+        print("Connected to database successfully")
+    except Exception as e:
+        print("Error during database connection: ", str(e))
+else:
+    print("Creating new database from schema backup...")
+    open('coinmarketcap_scraper_db.db', 'x')
+    try:
+        dbConn = sqlite3.connect('coinmarketcap_scraper_db.db')
+        print("Connected to database successfully")
+    except Exception as e:
+        print("Error during database connection: ", str(e))
 
+# creates database cursor to execute SQL scripts
+cursor = dbConn.cursor()
+
+# ensures proper tables exist within the database
+dbSchema = open('coinmarketcap_scraper_db_schema.sql')
+dbSchemaString = dbSchema.read()
+cursor.executescript(dbSchemaString)
 
 # define target URL to scrape
 url_to_scrape = "https://coinmarketcap.com/"
@@ -21,6 +46,7 @@ driver.get(url_to_scrape)
 for i in range(0, 8):
     driver.execute_script("window.scrollBy({top:1500,left:0,behavior: 'smooth'})")
     time.sleep(1)
+    print(f"Parsing chunk {i}/7...")
 page_html = driver.page_source
 driver.quit()
 
@@ -32,14 +58,23 @@ currency_items = html_soup.find('body').find('table').find_all('tr')
 results = 'top100.csv'
 f = open(results, 'w')
 
-# write header to results file
-header = 'Rank#, Name, Price, 24h %, 7d %, Market Cap, Volume (24h), Circulating Supply \n'
+# write timestamp and header to results file
+dtCurrent = 'TIMESTAMP: {:%Y-%b-%d %H:%M:%S}\n'.format(datetime.datetime.now())
+header = 'Rank#, Name, Symbol, Price, 24h %, 7d %, Market Cap, Volume (24h), Circulating Supply \n'
+f.write(dtCurrent)
 f.write(header)
 
 # scrape currency data for each and write to file
 for i in range(1, len(currency_items)):
     # name scrape:
     name = currency_items[i].find_all('p')[1].text
+
+    # prints a line to reveal current currency being scraped
+    print("********")
+    print(f"Scraping {name}")
+
+    # symbol scrape
+    symbol = currency_items[i].find_all('p')[2].text
 
     # price scrape:
     price = currency_items[i].find_all('a')[1].text
@@ -51,7 +86,7 @@ for i in range(1, len(currency_items)):
     svnDayDeltaP = currency_items[i].find_all('span')[4].text
 
     # Market Cap scrape:
-    mCap = currency_items[i].find_all('p')[3].text
+    mCap = currency_items[i].find_all('p')[3].find_all('span')[1].text
 
     # Day Volume scrape:
     dayVol = currency_items[i].find_all('p')[4].text
@@ -59,8 +94,27 @@ for i in range(1, len(currency_items)):
     # Circulating Supply scrape:
     cSupply = currency_items[i].find_all('p')[5].text
 
-    # writes data for each currency to results file
-    f.write(f"#{i}:, {name}, {price}, {oneDayDeltaP}, {svnDayDeltaP}, {mCap}, {dayVol}, {cSupply}\n")
+    # writes data for each currency to results file 
+    f.write(f"#{i}:, {name}, {symbol}, {price}, {oneDayDeltaP}, {svnDayDeltaP}, {mCap}, {dayVol}, {cSupply}\n")
+
+    # inserts data for each currency to database tables (expect errors from attempts to save duplicate entries to "currencies" data)
+    try:
+        cursor.execute(f'INSERT INTO "main"."currencies"("name","symbol") VALUES ("{name}","{symbol}")')
+    except Exception as e:
+        print(str(e))
+        print(f'{name} already exists in "currencies" table...ignoring')
+
+    cursor.execute(f'INSERT INTO "main"."market_data"("price","24h%","7d%","market_cap","volume_24h","supply","symbol") VALUES ("{price}","{oneDayDeltaP}","{svnDayDeltaP}","{mCap}","{dayVol}","{cSupply}","{symbol}");')
+
+    # prints a line on completion of each currency scrape
+    print(f"Finished scraping {name}!")
+    print("********")
 
 # closes the results file
 f.close()
+
+# closes database connection
+if dbConn:
+    dbConn.commit()
+    dbConn.close()
+    print("Connection to database has been closed")
